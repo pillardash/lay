7c020d46acadbb6305c82065aa71d560c5bc5bf3
Osahenrumwen Aigbogun <leonardosahon@gmail.com>
2025-08-20 11:29:13 +0100

    Updated boilerplate to start with apache as the deployment server

--- web/domains/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-include_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "index.php";
--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
 use BrickLayer\Lay\Core\View\Domain;
+use BrickLayer\Lay\Core\View\Enums\DomainType;
 
 if(!defined("SAFE_TO_INIT_LAY"))
     define("SAFE_TO_INIT_LAY", true);
@@ -9,11 +10,13 @@ include_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "found
 Domain::new()->create(
     id: "api-endpoint",
     builder: \Web\Api\Plaster::class,
-    patterns: ["api"],
+    pattern: "api",
+    type: DomainType::SPECIAL,
 );
 
 Domain::new()->create(
     id: "default",
     builder: \Web\Default\Plaster::class,
-    patterns: ["*"],
+    pattern: "*",
+    type: DomainType::REGULAR,
 );
